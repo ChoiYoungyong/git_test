3b41d21c08aca71f6116b055e07c740c6c361311
ChoiYoungyong <demoniku001@gmail.com>
2021-10-27 10:15:15 +0900

    for conflict at local

--- git_python.py
+++ git_python.py
@@ -2,18 +2,7 @@
 import numpy as np
 
 ## First written at Github
-int x, y
-x = rand(6)
-y = rand(100)
+x = [1, 2, 3, 4]
 
-## For Conflict at Github
-### Amend at Local follow to Github
-int z
-calc(x , y){
-  if x>=y
-  z = x - y
-  print(z)
-  else
-  z = x + y
-  print(z)
-}
+## Second written at local
+y = np.exp(x)
